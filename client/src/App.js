5db3202beed1f62103520781d6bb3cc6edbbc706
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2023-12-10 01:35:48 -0500

    some stuff

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,24 @@
 import { React, useEffect, useState, useRef } from 'react';
 import './style/App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowsRotate, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faArrowsRotate, faCaretDown, faClose } from '@fortawesome/free-solid-svg-icons'
 
 const App = (prop) => {
+  const Alert = (prop) => {
+    return (
+      <div className='alert'>
+        <FontAwesomeIcon icon={faClose} className='close' onClick={(e) => {
+          e.preventDefault();
+          e.target.parentElement.style.top = "200dvh";
+        }} />
+        <h1>{prop.title}</h1>
+        <hr/><br/>
+        <p>
+          {prop.info}
+        </p>
+      </div>
+    )
+  }
   var [Status, setStatus] = useState("Home");
   const [Output, setOutput] = useState([]);
   const generateNewID = () => {
@@ -60,6 +75,8 @@ const App = (prop) => {
   })
   return (
     <div className='menu'>
+
+      <Alert title="Important!!" info="Please verify that you have the 'override wallpaper' setting enabled to use this plugin. It can be found in the general settings tab." />
 
       <div id='syncing'>
         <FontAwesomeIcon icon={faCaretDown} className='close' onClick={(e) => {
